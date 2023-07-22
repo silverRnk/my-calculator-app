a910d1946fbd3567c9baad236da49216551bfa3e
pdbautista0 <patdi.bautista@gmail.com>
2023-07-22 20:28:17 +0800

    Created RestApi

--- src/routes/api/v1/compute/+server.js
+++ src/routes/api/v1/compute/+server.js
@@ -1,5 +1,17 @@
-export async function GET() {
-  return new Response(JSON.stringify({ message: "hello world" }), {
-    headers: { "Content-Type": "application/json" },
-  });
+import { error, json } from '@sveltejs/kit';
+import { SimpleCalculator } from '$lib/utils/Calculator';
+
+/** @type {import('./$types').RequestHandler} */
+export async function GET({url}) {
+  const expression = url.searchParams.get('exp') ?? ""
+  let expressionArray = expression.split(" ")
+
+
+  try{
+    let answer;
+    answer = SimpleCalculator.calculate(expressionArray);
+    return json({answer:answer})
+  }catch(e){
+    return json({error: 'Invalid'}, {status:401})
+  }
 }
